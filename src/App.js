9c1581812bc4448ae99b3f634611e2b22d8b8b53
Nikita <nikita@platform500.com>
2021-11-29 02:12:04 +0300

    +

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  {
+    id: 1,
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imgUrl: '/img/sneakers/1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15600,
+    imgUrl: '/img/sneakers/2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imgUrl: '/img/sneakers/3.jpg',
+  },
+  {
+    id: 4,
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 9000,
+    imgUrl: '/img/sneakers/4.jpg',
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,10 +43,15 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              key={obj.id}
+              title={obj.title}
+              price={obj.price}
+              imgUrl={obj.imgUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
